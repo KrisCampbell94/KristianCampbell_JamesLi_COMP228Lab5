ed16cb27f76f2dcacfe59e9a761e94913e204dc0
jchli07 <james.ch.li07@gmail.com>
2018-08-05 00:33:44 -0400

    Made textboxes and combobox for appropriate buttons

--- src/jswing_guitest/jswingLayout.java
+++ src/jswing_guitest/jswingLayout.java
@@ -22,6 +22,18 @@ public class jswingLayout {
         JButton buttonConfirm = new JButton("Confirm");
         buttonConfirm.setAlignmentY(Component.BOTTOM_ALIGNMENT);
 
+        String[] players = new String[] {"1", "2", "3", "4"};
+        JComboBox<String> comboPlayers = new JComboBox<String>(players);
+        comboPlayers.setAlignmentY(Component.TOP_ALIGNMENT);
+        comboPlayers.setSize(10,10);
+
+        JTextField fName = new JTextField("First Name");
+        JTextField lName = new JTextField("Last Name");
+        JTextField address = new JTextField("Address");
+        JTextField pCode = new JTextField("Postal Code");
+        JTextField province = new JTextField("Province (2 letters)");
+        JTextField phone = new JTextField("Phone Number");
+
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new FlowLayout());
         topPanel.add(buttonA);
@@ -35,27 +47,55 @@ public class jswingLayout {
 
         Box aBox = Box.createVerticalBox();
         //aBox.setLayout(new BoxLayout(aBox,BoxLayout.LINE_AXIS));
-        aBox.setPreferredSize(new Dimension(200,300));
+        aBox.setPreferredSize(new Dimension(100,100));
 
         JLabel label = new JLabel("Nothing to display here");
         buttonA.addActionListener(
                 e -> {
                     label.setText("INSERT");
                     label.setFont( new Font("serif", Font.PLAIN, 20));
-                    aBox.setPreferredSize(new Dimension(200,300));
+                    fName.setVisible(true);
+                    lName.setVisible(true);
+                    address.setVisible(true);
+                    pCode.setVisible(true);
+                    province.setVisible(true);
+                    phone.setVisible(true);
+                    buttonConfirm.setVisible(true);
+                    aBox.add(fName);
+                    aBox.add(lName);
+                    aBox.add(address);
+                    aBox.add(pCode);
+                    aBox.add(province);
+                    aBox.add(phone);
                     aBox.add(buttonConfirm);
+                    comboPlayers.setVisible(false);
+                    aBox.remove(comboPlayers);
                 }
         );
         buttonB.addActionListener(
                 e -> {
                     label.setText("UPDATE");
                     label.setFont( new Font("serif", Font.PLAIN, 20));
-                    aBox.setPreferredSize(new Dimension(200,300));
+                    fName.setVisible(false);
+                    lName.setVisible(false);
+                    address.setVisible(false);
+                    pCode.setVisible(false);
+                    province.setVisible(false);
+                    phone.setVisible(false);
+
+                    comboPlayers.setVisible(true);
+                    aBox.add(comboPlayers);
+
+                    buttonConfirm.setVisible(true);
                     aBox.add(buttonConfirm);
                 }
         );
         buttonC.addActionListener(
-                e -> label.setText("DISPLAY")
+                e -> {
+                    label.setText("DISPLAY");
+                    buttonConfirm.setVisible(false);
+                    aBox.remove(buttonConfirm);
+                }
         );
 
 
